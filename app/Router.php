071967f373494e8d10eb60c036c4bb97b010e949
mark <mtotubalin@mail.ru>
2020-09-04 11:35:52 +0300

    make search current route

--- app/Router.php
+++ app/Router.php
@@ -38,6 +38,65 @@ final class Router
     }
 
     /**
+     * @return string
+     */
+    public static function getCurrentPath(): string
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * @return string
+     */
+    public static function getCurrentMethod(): string
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    /**
+     * @return Route
+     */
+    public function findCurrentRoute(): Route
+    {
+        foreach ($this->routes as $route) {
+            if (false === $this->check($route)) {
+                continue;
+            }
+
+            return $route;
+        }
+
+        // TODO: make route for 404
+    }
+
+    /**
+     * @param Route $route
+     * @return bool
+     */
+    private function check(Route $route): bool
+    {
+        return $this->checkRoute($route) && $this->checkMethod($route);
+    }
+
+    /**
+     * @param Route $route
+     * @return bool
+     */
+    private function checkRoute(Route $route): bool
+    {
+        return (bool)preg_match($route->getPattern(), self::getCurrentPath());
+    }
+
+    /**
+     * @param Route $route
+     * @return bool
+     */
+    private function checkMethod(Route $route): bool
+    {
+        return self::getCurrentMethod() === $route->getMethod() || Route::METHOD_ANY === $route->getMethod();
+    }
+
+    /**
      * @param Route $route
      * @return Route
      */
